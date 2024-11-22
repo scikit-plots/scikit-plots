e825c1c99e89a137f5bac9c56a6000365e803200
Muhammed ÇELİK <developerhost01@gmail.com>
2024-11-22 15:52:59 +0300

    Release version 0.4.0.post0

--- scikitplot/_numcpp_api/src/version.cpp
+++ scikitplot/_numcpp_api/src/version.cpp
@@ -11,9 +11,11 @@
 // Without extern "C", name mangling is enabled,
 // and the function is only directly usable from C++.
 // C++ function declaration with a default message
-extern "C" std::string get_version() {
+extern "C" const char* get_version() {
   // Print the version
   // std::cout << nc::VERSION << std::endl;
   // Print the version
-  return nc::VERSION;  // Return the NumCpp version as a string
+  // return nc::VERSION;  // Return a string version as std::string
+  // Return the version as a C-style string
+  return nc::VERSION.c_str();  // .c_str() converts std::string to const char*
 }
