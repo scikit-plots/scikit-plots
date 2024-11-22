6fd38951c8f1a2eece1f7a192f0726df4d2b56a9
Muhammed ÇELİK <developerhost01@gmail.com>
2024-11-22 17:24:35 +0300

    Release version 0.4.0.post0

--- tools/wheels/repair_windows.sh
+++ tools/wheels/repair_windows.sh
@@ -1,3 +1,4 @@
+# Repairing wheel script
 set -xe
 
 WHEEL="$1"
@@ -11,7 +12,7 @@ pushd $DEST_DIR
 mkdir -p tmp
 pushd tmp
 wheel unpack $WHEEL
-pushd scikitplot*
+pushd scikit_plots*  # Adjust this to the correct unpacked directory name
 
 # To avoid DLL hell, the file name of libopenblas that's being vendored with
 # the wheel has to be name-mangled. delvewheel is unable to name-mangle PYD
@@ -19,7 +20,7 @@ pushd scikitplot*
 # building with mingw.
 # We therefore find each PYD in the directory structure and strip them.
 
-for f in $(find ./scikitplot* -name '*.pyd'); do strip $f; done
+for f in $(find ./scikit_plots* -name '*.pyd'); do strip $f; done
 
 
 # now repack the wheel and overwrite the original
