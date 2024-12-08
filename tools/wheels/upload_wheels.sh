0f767b222c3ba59afaa26453c3965d0f617d7172
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2024-12-08 04:31:12 +0300

    Updated

--- tools/wheels/upload_wheels.sh
+++ tools/wheels/upload_wheels.sh
@@ -1,5 +1,6 @@
 # Copied from numpy version
 # https://github.com/numpy/numpy/blob/main/tools/wheels/upload_wheels.sh
+# https://docs.anaconda.com/anacondaorg/user-guide/packages/standard-python-packages/
 
 set_travis_vars() {
     # Set env vars
@@ -21,13 +22,13 @@ set_upload_vars() {
     echo "IS_SCHEDULE_DISPATCH is $IS_SCHEDULE_DISPATCH"
     if [[ "$IS_PUSH" == "true" ]]; then
         echo push and tag event
-        export ANACONDA_ORG="scikit-plots-wheels-staging"
         export TOKEN="$SKPLT_STAGING_UPLOAD_TOKEN"
+        export USERNAME="scikit-plots-wheels-staging"
         export ANACONDA_UPLOAD="true"
     elif [[ "$IS_SCHEDULE_DISPATCH" == "true" ]]; then
         echo scheduled or dispatched event
-        export ANACONDA_ORG="scikit-plots-wheels-staging-nightly"
         export TOKEN="$SKPLT_NIGHTLY_UPLOAD_TOKEN"
+        export USERNAME="scikit-plots-wheels-staging-nightly"
         export ANACONDA_UPLOAD="true"
     else
         echo non-dispatch event
@@ -43,18 +44,22 @@ upload_wheels() {
             # sdists are located under dist folder when built through setup.py
             if compgen -G "./dist/*.gz"; then
                 echo "Found sdist"
-                anaconda -q -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./dist/*.gz
+                WHEEL_FILE_PATH = "./dist/*.gz"
+                anaconda -t ${TOKEN} -u ${USERNAME} --force -q upload ${WHEEL_FILE_PATH}
             elif compgen -G "./wheelhouse/*.whl"; then
                 echo "Found wheel"
                 # Force a replacement if the remote file already exists -
                 # nightlies will not have the commit ID in the filename, so
                 # are named the same (1.X.Y.dev0-<platform/interpreter-tags>)
-                anaconda -q -t ${TOKEN} upload --force -u ${ANACONDA_ORG} ./wheelhouse/*.whl
+                WHEEL_FILE_PATH = "./wheelhouse/*.whl"
+                anaconda -t ${TOKEN} -u ${USERNAME} --force -q upload ${WHEEL_FILE_PATH}
             else
                 echo "Files do not exist"
                 return 1
             fi
-            echo "PyPI-style index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"
+            # export PACKAGE="scikit-plots*"
+            # Your package is now available at http://anaconda.org/<USERNAME>/<PACKAGE>
+            echo "PyPI-style index: https://pypi.anaconda.org/$USERNAME/simple"
         fi
     fi
 }
