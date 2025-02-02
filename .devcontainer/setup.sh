274a082bbf3ea418b89126128d91a2e41226ba15
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2025-02-02 14:38:27 +0300

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,7 +7,11 @@ set -e
 conda init --all
 micromamba shell init -s bash
 micromamba env create -f environment.yml --yes
-# Note that `micromamba activate mpl-dev` doesn't work, it must be run by the
+# Note that `micromamba activate scipy-dev` doesn't work, it must be run by the
 # user (same applies to `conda activate`)
+
+git submodule update --init
+
+# Enables users to activate environment without having to specify the full path
 echo "envs_dirs:
   - /home/codespace/micromamba/envs" > /opt/conda/.condarc
