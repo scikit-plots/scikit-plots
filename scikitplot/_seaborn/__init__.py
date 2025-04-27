ad92d285b9b15db7ef1d044d59d2ce8cbc44a657
Muhammed ÇELİK <developerhost01@gmail.com>
2025-04-27 15:03:46 +0000

    fix astopy stats Quantity

--- scikitplot/_seaborn/__init__.py
+++ scikitplot/_seaborn/__init__.py
@@ -8,10 +8,8 @@ Documentation is available in the docstrings and
 online at https://seaborn.pydata.org/.
 """
 
-# Authors: The scikit-plots developers
-# SPDX-License-Identifier: BSD-3-Clause
-
 # scikitplot/_seaborn/__init__.py
+# https://github.com/mwaskom/seaborn/blob/master/seaborn/__init__.py
 
 # Import seaborn objects
 from .rcmod import *  # noqa: F401,F403
@@ -28,22 +26,23 @@ from .widgets import *  # noqa: F401,F403
 from .colors import xkcd_rgb, crayons  # noqa: F401
 from . import cm  # noqa: F401
 
-from .._build_utils.gitversion import git_remote_version
-
 # Capture the original matplotlib rcParams
 import matplotlib as mpl
 
 _orig_rc_params = mpl.rcParams.copy()
+
+# Define the seaborn version
+__version__ = "0.14.0.dev0"
+
+#######################################################################
+
 del mpl
 
 __author__ = "Michael Waskom"
 __author_email__ = "mwaskom@gmail.com"
 
 # Define the seaborn git hash
-# scikitplot._build_utils.gitversion.git_remote_version(url='https://github.com/mwaskom/seaborn')[0]
+from .._build_utils.gitversion import git_remote_version
+
 __git_hash__ = git_remote_version(url="https://github.com/scikit-plots/seaborn")[0]
 del git_remote_version
-
-# Define the seaborn version
-# https://github.com/mwaskom/seaborn/blob/master/seaborn/__init__.py
-# __version__ = "0.14.0.dev0"
