4e2b67875cac3be6a41e6fb8b197cdb5c296fa69
Muhammed ÇELİK <developerhost01@gmail.com>
2025-04-29 17:08:03 +0000

    fix meson

--- .github/scripts/utils.sh
+++ .github/scripts/utils.sh
@@ -2,6 +2,42 @@
 ######################################################################
 ## Logging Functions
 ######################################################################
+# ANSI color codes for terminal output:
+# \033[0m       - Reset / Normal
+# \033[1m       - Bold
+# \033[2m       - Dim
+# \033[4m       - Underline
+
+# Foreground colors:
+# \033[30m      - Black
+# \033[31m      - Red
+# \033[32m      - Green
+# \033[33m      - Yellow
+# \033[34m      - Blue
+# \033[35m      - Magenta
+# \033[36m      - Cyan
+# \033[37m      - White
+
+# Bright foreground colors:
+# \033[1;30m    - Bright Black (Gray)
+# \033[1;31m    - Bright Red
+# \033[1;32m    - Bright Green
+# \033[1;33m    - Bright Yellow
+# \033[1;34m    - Bright Blue
+# \033[1;35m    - Bright Magenta
+# \033[1;36m    - Bright Cyan
+# \033[1;37m    - Bright White
+
+# Background colors (optional, less common):
+# \033[40m      - Background Black
+# \033[41m      - Background Red
+# \033[42m      - Background Green
+# \033[43m      - Background Yellow
+# \033[44m      - Background Blue
+# \033[45m      - Background Magenta
+# \033[46m      - Background Cyan
+# \033[47m      - Background White
+
 # Colors for Pretty Logs
 # RESET="\033[0m"
 # CYAN="\033[1;36m"
