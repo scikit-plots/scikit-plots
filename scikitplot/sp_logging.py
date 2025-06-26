609d5c74adccee3238ac529d6caddac5aab3637a
Muhammed ÇELİK <developerhost01@gmail.com>
2025-06-26 12:07:48 +0000

    Docs build error

--- scikitplot/sp_logging.py
+++ scikitplot/sp_logging.py
@@ -889,8 +889,8 @@ def get_logger() -> "_logging.Logger":
 
     See Also
     --------
-    scikitplot.logger :
-        An instance of :py:mod:`sp_logging` class, providing logging logger functionality.
+    logger :
+        An alias of :py:mod:`sp_logging` module, providing logging functionality.
     logging.getLogger :
         Standard library function to retrieve :py:class:`logging.Logger` instance.
         For more: https://docs.python.org/3/library/logging.html
@@ -1372,8 +1372,8 @@ class SpLogger:
     --------
     get_logger :
         Function that provides a shared :py:class:`logging.Logger` instance.
-    scikitplot.logger :
-        An instance of :py:mod:`sp_logging` class, providing logging functionality.
+    logger :
+        An alias of :py:mod:`sp_logging` module, providing logging functionality.
     logging.getLogger :
         Standard library function to retrieve :py:class:`logging.Logger` instance,
         for more https://docs.python.org/3/library/logging.html.
