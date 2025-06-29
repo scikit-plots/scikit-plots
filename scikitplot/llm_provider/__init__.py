2c834861d565a0ba9dc29fd8ed176e01ea2a551b
Muhammed ÇELİK <developerhost01@gmail.com>
2025-06-29 10:59:36 +0000

    docker

--- scikitplot/llm_provider/__init__.py
+++ scikitplot/llm_provider/__init__.py
@@ -1,11 +1,24 @@
 # Authors: The scikit-plots developers
 # SPDX-License-Identifier: BSD-3-Clause
 
-"""llm_provider."""
+"""
+:py:mod:`~.llm_provider` for Large Language Models.
+
+.. seealso::
+
+   * https://huggingface.co/scikit-plots
+   * https://huggingface.co/spaces/scikit-plots/model-sight
+"""
 
 from .._testing._pytesttester import PytestTester  # Pytest testing
-from ..utils.utils_mlflow import load_mlflow_gateway_config  # noqa: F401
-from .model_registry import (  # noqa: F401
+from ..utils.utils_mlflow import load_mlflow_gateway_config
+from . import (
+    chat_provider,
+    clint_provider,
+    model_registry,
+)
+from .chat_provider import get_response
+from .model_registry import (
     LLM_MODEL_PROVIDER2API_KEY,
     LLM_MODEL_PROVIDER2CONFIG,
 )
@@ -18,6 +31,10 @@ __all__ = [
     # 'model_registry',
     "LLM_MODEL_PROVIDER2API_KEY",
     "LLM_MODEL_PROVIDER2CONFIG",
+    "chat_provider",
+    "clint_provider",
+    "get_response",
     "load_mlflow_gateway_config",
+    "model_registry",
     "test",
 ]
