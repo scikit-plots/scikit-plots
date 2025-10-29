6b5c59fa2630279d1739bc175e8fd53f1d05d7de
celik-muhammed <developerhost01@gmail.com>
2025-10-29 05:11:56 +0000

    Updated files (c)externals.

--- tools/submodules/vendored_astropy/__init__.py
+++ tools/submodules/vendored_astropy/__init__.py
@@ -14,9 +14,53 @@ online at https://www.astropy.org/.
 
 #######################################################################
 
-from ._version import __version__
+from .version import version as __version__
 
 # Define the astropy version
 __author__ = "Astropy Developers"
 __author_email__ = "astropy.team@gmail.com"
 __git_hash__  = "dbc384f3eeff4576b41a68486fcbb0a77789a8d8"
+
+__all__ = [  # noqa: RUF100, RUF022
+    "__version__",
+    # "__bibtex__",
+    # # Subpackages (mostly lazy-loaded)
+    # "config",
+    # "constants",
+    # "convolution",
+    # "coordinates",
+    # "cosmology",
+    # "io",
+    # "modeling",
+    # "nddata",
+    # "samp",
+    "stats",
+    # "table",
+    # "tests",
+    # "time",
+    # "timeseries",
+    # "uncertainty",
+    # "units",
+    # "utils",
+    # "visualization",
+    # "wcs",
+    # # Functions
+    # "test",
+    # "log",
+    # "find_api_page",
+    # "online_help",
+    # "online_docs_root",
+    # "conf",
+    # "physical_constants",
+    # "astronomical_constants",
+    # "system_info",
+]
+
+
+def __getattr__(attr):
+    if attr in __all__:
+        from importlib import import_module
+
+        return import_module("scikitplot.cexternals._astropy." + attr)
+
+    raise AttributeError(f"submodule '_astropy' has no attribute {attr!r}")
